cee0cf5251bb5c3f7b53ab18ea7480be5f2df3f1
danenglish3 <42049691+danenglish3@users.noreply.github.com>
2024-10-01 20:56:17 +1300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        DATABASE_URL: process.env.DATABASE_URL,
+        DATABASE_URL: "postgres://default:Uzhw3lDK1OtM@ep-muddy-grass-a7uwclke.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require"
     },
     eslint: {
         ignoreDuringBuilds: true,
